46156c7326e0f91481a72ce1afe0e16ed7f7617d
Josh Gao <josh_gao@foxmail.com>
2023-04-12 00:33:20 +0800

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,17 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `gatsby-josh`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site 1",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
