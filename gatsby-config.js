e8e67574949888f15cc3dedc18d8076284af0a6d
Josh Gao <josh_gao@foxmail.com>
2023-04-11 21:56:45 +0800

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `gatsby-josh`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
